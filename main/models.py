d6f50a00b58c33b451c7f289f1680675c72489ad
Kirza <kirzagroup@gmail.com>
2020-10-25 13:41:22 +0000

    Modified the models field to add the related for foreign key and views.

--- main/models.py
+++ main/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 
 class Investment(models.Model):
     date = models.DateField(default=timezone.now)
-    investor = models.ForeignKey(User, on_delete=models.CASCADE)
+    investor = models.ForeignKey(User, on_delete=models.CASCADE, related_name='investments')
     amount = models.DecimalField(decimal_places=2, max_digits=1000)
     notes = models.CharField(max_length=200, default=0, blank=True)
 
--- main/views.py
+++ main/views.py
@@ -1,20 +1,16 @@
 from django.shortcuts import render, redirect
-from .models import Investment
 from django.contrib.auth.decorators import login_required
-
 
 
 @login_required(login_url='accounts:login')
 def home(request):
-    # context = {}
-    # return render(request, 'base.html', context)
     return redirect('main:investment_list')
 
 
 @login_required(login_url='accounts:login')
 def investment_list(request):
-    current_user = request.user 
-    user_investments = Investment.objects.filter(investor=current_user).order_by('date')
+    current_user = request.user
+    user_investments = current_user.investments.all().order_by('date')
     total_investments = 0
     if user_investments:
         for obj in user_investments:
